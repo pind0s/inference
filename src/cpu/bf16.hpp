b65b5f08582318dcfc0e1d227618324ec2e800fb
pind0s <69063577+pind0s@users.noreply.github.com>
2026-08-09 14:20:54 -0500

    feat(cpu): add bf16 type

---
+++ src/cpu/bf16.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <immintrin.h>
+
+namespace inference::cpu {
+    struct bf16_t {
+        __bf16 value;
+
+        // todo maybe we should just use cast directly? need to look into it
+        [[gnu::always_inline]] [[nodiscard]] static bf16_t from_float(const float f32) {
+            bf16_t result {};
+            result.value = _mm_cvtness_sbh(f32);
+            return result;
+        }
+
+        // todo think about removing this, since its just a simple C cast return (float)(__A);
+        [[gnu::always_inline]] [[nodiscard]] float to_float() const {
+            return _mm_cvtsbh_ss(value);
+        }
+    };
+} // namespace inference::cpu
